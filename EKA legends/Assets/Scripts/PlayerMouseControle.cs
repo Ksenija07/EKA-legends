84c5346d6d7f0e6dcadd0d9755c70c8ea4135951
Ksenija07 <ksenijabidzane@gmail.com>
2025-04-10 12:10:13 +0300

    ModelesMainCharacter

---
+++ EKA legends/Assets/Scripts/PlayerMouseControle.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseControle : MonoBehaviour
+{
+    
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float moveSpeed = 10;
+    private CharacterController characterController;
+    private Vector3 targetPosition;
+    private Animator animator;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        targetPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float distToTarget = Vector3.Distance(transform.position, targetPosition);
+        if (distToTarget > 1f)
+        {
+            Vector3 targetDirection = Vector3.Normalize(targetPosition - transform.position);
+            characterController.Move(targetDirection * moveSpeed * Time.deltaTime);
+            transform.LookAt(targetPosition);
+            animator.SetBool("Running", true);
+
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, 500,layerMask))
+            {
+                Debug.Log("hit: " + hit.collider.name);
+                targetPosition = hit.point;
+            }
+        }
+    }
+}
